e9cd3a2871fca1eb4473c78c5bd2742909a7cd74
Vsevolod Bezaev <vvbezaev@gmail.com>
2021-03-17 18:55:29 +0300

    Made changes to profile to be done in one url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import ProfileAPIView, OtherProfileAPIView
+from .views import ProfileAPIView
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('profiles/<str:username>', OtherProfileAPIView.as_view()),
-    url('profile', ProfileAPIView.as_view()),
+    url(r'^profiles(?:/(?P<username>.+))?/$', ProfileAPIView.as_view())
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import PermissionDenied
 
-from rest_framework.generics import RetrieveAPIView, RetrieveUpdateAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -11,30 +12,28 @@ from authentication.renderers import UserJSONRenderer
 from .serializers import UserWithProfileSerializer
 
 
-class OtherProfileAPIView(RetrieveAPIView):
-    permission_classes = (AllowAny,)
+class ProfileAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = UserWithProfileSerializer
 
-    def retrieve(self, request, username):
-        user = get_object_or_404(User, username=username)
+    def retrieve(self, request, username=None, *args, **kwargs):
+        if username is not None:
+            user = get_object_or_404(User, username=username)
+        else:
+            user = request.user
         serializer = self.serializer_class(user)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class ProfileAPIView(RetrieveUpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    renderer_classes = (UserJSONRenderer,)
-    serializer_class = UserWithProfileSerializer
-
-    def retrieve(self, request, *args, **kwargs):
-        serializer = self.serializer_class(request.user)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, *args, **kwargs):
+    def update(self, request, username=None, *args, **kwargs):
         user_data = request.data.get('user', {})
-        serializer = self.serializer_class(request.user, partial=True, data=user_data)
+        if (username is not None) and (username != request.user.username):
+            raise PermissionDenied()
+        else:
+            user = request.user
+
+        serializer = self.serializer_class(user, partial=True, data=user_data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         
